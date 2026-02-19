c09f71fcdda96afd152986eb68aedd73e4307d56
Alejo <20245281@esen.edu.sv>
2026-02-19 08:37:38 -0600

    controller

--- app/Http/Controllers/LibrayController.php
+++ app/Http/Controllers/LibrayController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BookResource;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class LibrayController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Lógica para obtener la lista de libros
+        $query = Book::query();
+
+        if ($request->has('title')) {
+            $query->where('title', 'like', '%' . $request->input('title') . '%');
+        }
+
+        if ($request->has('ISBN')) {
+            $query->where('ISBN', '=', $request->ISBN);
+        }
+
+        if ($request->has('is_available')) {
+            $isAvailable = filter_var($request->is_available, FILTER_VALIDATE_BOOLEAN);
+            $query->where('is_available', '=', $isAvailable);
+        }
+
+        $books = $query->paginate(15);
+        return BookResource::collection($books);
     }
 
     public function store(Request $request)
