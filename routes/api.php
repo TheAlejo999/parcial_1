7b71d14bfa3ff38f797f4bc64f0412a8377ab4ae
Alejo <20245281@esen.edu.sv>
2026-02-19 09:21:15 -0600

    api

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibrayController;
+use App\Http\Controllers\LoanController;
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::get('/users', 'LibraryController@index');
-    Route::post('/users', 'LibraryController@store');
-    Route::post('/users/{id}', 'LibraryController@update');
+Route::get('/users', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('books')->group(function () {
+    Route::get('/', [LibrayController::class, 'index'])->name('books.index');
+    Route::get('/{book}', [LibrayController::class, 'show'])->name('books.show');
 });
+
+Route::prefix('loans')->group(function () {
+    Route::post('/', [LoanController::class, 'store'])->name('loans.store');
+    Route::get('/', [LoanController::class, 'index'])->name('loans.index');
+});
+
+Route::post('/returns/{loan_id}', [LoanController::class, 'return'])->name('loans.return');
