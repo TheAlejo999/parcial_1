bfee034796db1755f8c23cb3f7675948cc711ed1
Alejo <20245281@esen.edu.sv>
2026-02-19 09:15:18 -0600

    loans

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -45,5 +45,59 @@ class LoanController extends Controller
                 'book_title' => $book->title,
             ]
         ], response::HTTP_CREATED);
-    }         
+    }    
+    public function return($loanId): JsonResponse
+        {
+            $loan = Loan::findOrFail($loanId);
+           if ($loan->isReturned()) {
+                return response()->json(['message' => 'Este préstamo ya ha sido devuelto.'], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $loan->return_date = now();
+            $loan->save();
+
+            $book = $loan->book;
+
+            $book->available_copies += 1;
+
+            if ($book->is_available && $book->available_copies > 0) {
+                $book->is_available = true;
+            }
+
+            $book->save();
+
+
+            return response()->json(
+                ['message' => 'Libro devuelto exitosamente.',
+                'loan'=> [
+                    'id' => $loan->id,
+                    'solicitor_name' => $loan->solicitor_name,
+                    'loan_date' => $loan->loan_date,
+                    'return_date' => $loan->return_date,
+                    'book_id' => $loan->book_id,
+                    'book_title' => $book->title,
+                ]
+                ], Response::HTTP_OK);
+    }
+
+    public function index()
+    {
+        $loans = Loan::with('book')->get()->map(function ($loan) {
+            return [
+                'id' => $loan->id,
+                'solicitor_name' => $loan->solicitor_name,
+                'loan_date' => $loan->loan_date,
+                'return_date' => $loan->return_date,
+                'is_available' => $loan->isReturned() ? 'Devuelto' : 'Activo',
+                'book' => [
+                    'id' => $loan->book->id,
+                    'title' => $loan->book->title,
+                    'ISBN' => $loan->book->ISBN,
+                ],
+            ];
+        });
+
+        return response()->json(
+            ['loans' => $loans,
+            'total' => $loans->count()], Response::HTTP_OK);
+    }
 }
